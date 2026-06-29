1d5970deb7d54ddfd77b73c7ea7d62f5aca54701
divyanshupandey808-crypto <divyanshupandey808@gmail.com>
2026-06-28 23:19:42 -0700

    Update product.js

--- product.js
+++ product.js
@@ -1,54 +1,25 @@
 const params = new URLSearchParams(window.location.search);
-
 const code = params.get("code");
 
 const product = products.find(p => p.code === code);
 
-const productImage = document.getElementById("productImage");
-const productCode = document.getElementById("productCode");
-const productCategory = document.getElementById("productCategory");
-const whatsappBtn = document.getElementById("whatsappBtn");
+const img = document.getElementById("productImage");
+const codeBox = document.getElementById("productCode");
+const catBox = document.getElementById("productCategory");
 
 if(product){
-
-productImage.src = product.image;
-
-productCode.textContent = product.code;
-
-productCategory.textContent = product.category.toUpperCase();
-
-const message =
-`Hello, I want to buy Product ${product.code}`;
-
-whatsappBtn.href =
-`https://wa.me/919507059053?text=${encodeURIComponent(message)}`;
-
-}else{
-
-document.body.innerHTML = `
-<h2 style="text-align:center;margin-top:100px;">
-Product Not Found
-</h2>
-`;
-
+img.src = product.image;
+codeBox.innerText = product.code;
+catBox.innerText = product.category;
 }
 
-// Image Zoom
-
-productImage.addEventListener("click",()=>{
-
-if(productImage.style.transform==="scale(2)"){
-
-productImage.style.transform="scale(1)";
-
-productImage.style.cursor="zoom-in";
-
-}else{
-
-productImage.style.transform="scale(2)";
-
-productImage.style.cursor="zoom-out";
-
+function addToCart(){
+let cart = JSON.parse(localStorage.getItem("cart")) || [];
+cart.push(product);
+localStorage.setItem("cart", JSON.stringify(cart));
+alert("Added to cart!");
 }
 
+img.addEventListener("click", () => {
+img.classList.toggle("zoom");
 });
