bccaf77275ea51780e7a5af604a73da2680d7fa7
divyanshupandey808-crypto <divyanshupandey808@gmail.com>
2026-06-28 19:29:02 -0700

    Create products.js

---
+++ products.js
@@ -0,0 +1,1482 @@
+const products = [
+
+{
+  code: "YOM001",
+  category: "women",
+  image: "images/yom001.jpg"
+},
+
+{
+  code: "YOM002",
+  category: "women",
+  image: "images/yom002.jpg"
+},
+
+{
+  code: "YOM003",
+  category: "women",
+  image: "images/yom003.jpg"
+},
+
+{
+  code: "YOM004",
+  category: "women",
+  image: "images/yom004.jpg"
+},
+
+{
+  code: "YOM005",
+  category: "women",
+  image: "images/yom005.jpg"
+},
+
+{
+  code: "YOM006",
+  category: "women",
+  image: "images/yom006.jpg"
+},
+
+{
+  code: "YOM007",
+  category: "women",
+  image: "images/yom007.jpg"
+},
+
+{
+  code: "YOM008",
+  category: "women",
+  image: "images/yom008.jpg"
+},
+
+{
+  code: "YOM009",
+  category: "women",
+  image: "images/yom009.jpg"
+},
+
+{
+  code: "YOM010",
+  category: "women",
+  image: "images/yom010.jpg"
+},
+
+];const products = [
+
+{
+  code: "YOM011",
+  category: "women",
+  image: "images/yom011.jpg"
+},
+
+{
+  code: "YOM012",
+  category: "women",
+  image: "images/yom012.jpg"
+},
+
+{
+  code: "YOM013",
+  category: "women",
+  image: "images/yom013.jpg"
+},
+
+{
+  code: "YOM014",
+  category: "women",
+  image: "images/yom014.jpg"
+},
+
+{
+  code: "YOM015",
+  category: "women",
+  image: "images/yom015.jpg"
+},
+
+{
+  code: "YOM016",
+  category: "women",
+  image: "images/yom016.jpg"
+},
+
+{
+  code: "YOM017",
+  category: "women",
+  image: "images/yom017.jpg"
+},
+
+{
+  code: "YOM018",
+  category: "women",
+  image: "images/yom018.jpg"
+},
+
+{
+  code: "YOM019",
+  category: "women",
+  image: "images/yom019.jpg"
+},
+
+{
+  code: "YOM020",
+  category: "women",
+  image: "images/yom020.jpg"
+},
+
+];
+{
+  code: "YOM021",
+  category: "women",
+  image: "images/yom021.jpg"
+},
+
+{
+  code: "YOM022",
+  category: "women",
+  image: "images/yom022.jpg"
+},
+
+{
+  code: "YOM023",
+  category: "women",
+  image: "images/yom023.jpg"
+},
+
+{
+  code: "YOM024",
+  category: "women",
+  image: "images/yom024.jpg"
+},
+
+{
+  code: "YOM025",
+  category: "women",
+  image: "images/yom025.jpg"
+},
+
+{
+  code: "YOM026",
+  category: "women",
+  image: "images/yom026.jpg"
+},
+
+{
+  code: "YOM027",
+  category: "women",
+  image: "images/yom027.jpg"
+},
+
+{
+  code: "YOM028",
+  category: "women",
+  image: "images/yom028.jpg"
+},
+
+{
+  code: "YOM029",
+  category: "women",
+  image: "images/yom029.jpg"
+},
+
+{
+  code: "YOM030",
+  category: "women",
+  image: "images/yom030.jpg"
+},
+{
+  code: "YOM031",
+  category: "women",
+  image: "images/yom031.jpg"
+},
+
+{
+  code: "YOM032",
+  category: "women",
+  image: "images/yom032.jpg"
+},
+
+{
+  code: "YOM033",
+  category: "women",
+  image: "images/yom033.jpg"
+},
+
+{
+  code: "YOM034",
+  category: "women",
+  image: "images/yom034.jpg"
+},
+
+{
+  code: "YOM035",
+  category: "women",
+  image: "images/yom035.jpg"
+},
+
+{
+  code: "YOM036",
+  category: "women",
+  image: "images/yom036.jpg"
+},
+
+{
+  code: "YOM037",
+  category: "women",
+  image: "images/yom037.jpg"
+},
+
+{
+  code: "YOM038",
+  category: "women",
+  image: "images/yom038.jpg"
+},
+
+{
+  code: "YOM039",
+  category: "women",
+  image: "images/yom039.jpg"
+},
+
+{
+  code: "YOM040",
+  category: "women",
+  image: "images/yom040.jpg"
+},
+{
+  code: "YOM041",
+  category: "women",
+  image: "images/yom041.jpg"
+},
+
+{
+  code: "YOM042",
+  category: "women",
+  image: "images/yom042.jpg"
+},
+
+{
+  code: "YOM043",
+  category: "women",
+  image: "images/yom043.jpg"
+},
+
+{
+  code: "YOM044",
+  category: "women",
+  image: "images/yom044.jpg"
+},
+
+{
+  code: "YOM045",
+  category: "women",
+  image: "images/yom045.jpg"
+},
+
+{
+  code: "YOM046",
+  category: "women",
+  image: "images/yom046.jpg"
+},
+
+{
+  code: "YOM047",
+  category: "women",
+  image: "images/yom047.jpg"
+},
+
+{
+  code: "YOM048",
+  category: "women",
+  image: "images/yom048.jpg"
+},
+
+{
+  code: "YOM049",
+  category: "women",
+  image: "images/yom049.jpg"
+},
+
+{
+  code: "YOM050",
+  category: "women",
+  image: "images/yom050.jpg"
+},
+{
+  code: "YOM051",
+  category: "women",
+  image: "images/yom051.jpg"
+},
+
+{
+  code: "YOM052",
+  category: "women",
+  image: "images/yom052.jpg"
+},
+
+{
+  code: "YOM053",
+  category: "women",
+  image: "images/yom053.jpg"
+},
+
+{
+  code: "YOM054",
+  category: "women",
+  image: "images/yom054.jpg"
+},
+
+{
+  code: "YOM055",
+  category: "women",
+  image: "images/yom055.jpg"
+},
+
+{
+  code: "YOM056",
+  category: "women",
+  image: "images/yom056.jpg"
+},
+
+{
+  code: "YOM057",
+  category: "women",
+  image: "images/yom057.jpg"
+},
+
+{
+  code: "YOM058",
+  category: "women",
+  image: "images/yom058.jpg"
+},
+
+{
+  code: "YOM059",
+  category: "women",
+  image: "images/yom059.jpg"
+},
+
+{
+  code: "YOM060",
+  category: "women",
+  image: "images/yom060.jpg"
+},
+{
+  code: "YOM061",
+  category: "women",
+  image: "images/yom061.jpg"
+},
+
+{
+  code: "YOM062",
+  category: "women",
+  image: "images/yom062.jpg"
+},
+
+{
+  code: "YOM063",
+  category: "women",
+  image: "images/yom063.jpg"
+},
+
+{
+  code: "YOM064",
+  category: "women",
+  image: "images/yom064.jpg"
+},
+
+{
+  code: "YOM065",
+  category: "women",
+  image: "images/yom065.jpg"
+},
+
+{
+  code: "YOM066",
+  category: "women",
+  image: "images/yom066.jpg"
+},
+
+{
+  code: "YOM067",
+  category: "women",
+  image: "images/yom067.jpg"
+},
+
+{
+  code: "YOM068",
+  category: "women",
+  image: "images/yom068.jpg"
+},
+
+{
+  code: "YOM069",
+  category: "women",
+  image: "images/yom069.jpg"
+},
+
+{
+  code: "YOM070",
+  category: "women",
+  image: "images/yom070.jpg"
+},
+{
+  code: "YOM071",
+  category: "women",
+  image: "images/yom071.jpg"
+},
+
+{
+  code: "YOM072",
+  category: "women",
+  image: "images/yom072.jpg"
+},
+
+{
+  code: "YOM073",
+  category: "women",
+  image: "images/yom073.jpg"
+},
+
+{
+  code: "YOM074",
+  category: "women",
+  image: "images/yom074.jpg"
+},
+
+{
+  code: "YOM075",
+  category: "women",
+  image: "images/yom075.jpg"
+},
+
+{
+  code: "YOM076",
+  category: "women",
+  image: "images/yom076.jpg"
+},
+
+{
+  code: "YOM077",
+  category: "women",
+  image: "images/yom077.jpg"
+},
+
+{
+  code: "YOM078",
+  category: "women",
+  image: "images/yom078.jpg"
+},
+
+{
+  code: "YOM079",
+  category: "women",
+  image: "images/yom079.jpg"
+},
+
+{
+  code: "YOM080",
+  category: "women",
+  image: "images/yom080.jpg"
+},
+{
+  code: "YOM081",
+  category: "women",
+  image: "images/yom081.jpg"
+},
+
+{
+  code: "YOM082",
+  category: "women",
+  image: "images/yom082.jpg"
+},
+
+{
+  code: "YOM083",
+  category: "women",
+  image: "images/yom083.jpg"
+},
+
+{
+  code: "YOM084",
+  category: "women",
+  image: "images/yom084.jpg"
+},
+
+{
+  code: "YOM085",
+  category: "women",
+  image: "images/yom085.jpg"
+},
+
+{
+  code: "YOM086",
+  category: "women",
+  image: "images/yom086.jpg"
+},
+
+{
+  code: "YOM087",
+  category: "women",
+  image: "images/yom087.jpg"
+},
+
+{
+  code: "YOM088",
+  category: "women",
+  image: "images/yom088.jpg"
+},
+
+{
+  code: "YOM089",
+  category: "women",
+  image: "images/yom089.jpg"
+},
+
+{
+  code: "YOM090",
+  category: "women",
+  image: "images/yom090.jpg"
+},
+{
+  code: "YOM091",
+  category: "women",
+  image: "images/yom091.jpg"
+},
+
+{
+  code: "YOM092",
+  category: "women",
+  image: "images/yom092.jpg"
+},
+
+{
+  code: "YOM093",
+  category: "women",
+  image: "images/yom093.jpg"
+},
+
+{
+  code: "YOM094",
+  category: "women",
+  image: "images/yom094.jpg"
+},
+
+{
+  code: "YOM095",
+  category: "women",
+  image: "images/yom095.jpg"
+},
+
+{
+  code: "YOM096",
+  category: "women",
+  image: "images/yom096.jpg"
+},
+
+{
+  code: "YOM097",
+  category: "women",
+  image: "images/yom097.jpg"
+},
+
+{
+  code: "YOM098",
+  category: "women",
+  image: "images/yom098.jpg"
+},
+
+{
+  code: "YOM099",
+  category: "women",
+  image: "images/yom099.jpg"
+},
+
+{
+  code: "YOM100",
+  category: "women",
+  image: "images/yom100.jpg"
+},
+{
+  code: "YOM101",
+  category: "men",
+  image: "images/yom101.jpg"
+},
+
+{
+  code: "YOM102",
+  category: "men",
+  image: "images/yom102.jpg"
+},
+
+{
+  code: "YOM103",
+  category: "men",
+  image: "images/yom103.jpg"
+},
+
+{
+  code: "YOM104",
+  category: "men",
+  image: "images/yom104.jpg"
+},
+
+{
+  code: "YOM105",
+  category: "men",
+  image: "images/yom105.jpg"
+},
+
+{
+  code: "YOM106",
+  category: "men",
+  image: "images/yom106.jpg"
+},
+
+{
+  code: "YOM107",
+  category: "men",
+  image: "images/yom107.jpg"
+},
+
+{
+  code: "YOM108",
+  category: "men",
+  image: "images/yom108.jpg"
+},
+
+{
+  code: "YOM109",
+  category: "men",
+  image: "images/yom109.jpg"
+},
+
+{
+  code: "YOM110",
+  category: "men",
+  image: "images/yom110.jpg"
+},
+{
+  code: "YOM111",
+  category: "men",
+  image: "images/yom111.jpg"
+},
+
+{
+  code: "YOM112",
+  category: "men",
+  image: "images/yom112.jpg"
+},
+
+{
+  code: "YOM113",
+  category: "men",
+  image: "images/yom113.jpg"
+},
+
+{
+  code: "YOM114",
+  category: "men",
+  image: "images/yom114.jpg"
+},
+
+{
+  code: "YOM115",
+  category: "men",
+  image: "images/yom115.jpg"
+},
+
+{
+  code: "YOM116",
+  category: "men",
+  image: "images/yom116.jpg"
+},
+
+{
+  code: "YOM117",
+  category: "men",
+  image: "images/yom117.jpg"
+},
+
+{
+  code: "YOM118",
+  category: "men",
+  image: "images/yom118.jpg"
+},
+
+{
+  code: "YOM119",
+  category: "men",
+  image: "images/yom119.jpg"
+},
+
+{
+  code: "YOM120",
+  category: "men",
+  image: "images/yom120.jpg"
+},
+{
+  code: "YOM121",
+  category: "men",
+  image: "images/yom121.jpg"
+},
+
+{
+  code: "YOM122",
+  category: "men",
+  image: "images/yom122.jpg"
+},
+
+{
+  code: "YOM123",
+  category: "men",
+  image: "images/yom123.jpg"
+},
+
+{
+  code: "YOM124",
+  category: "men",
+  image: "images/yom124.jpg"
+},
+
+{
+  code: "YOM125",
+  category: "men",
+  image: "images/yom125.jpg"
+},
+
+{
+  code: "YOM126",
+  category: "men",
+  image: "images/yom126.jpg"
+},
+
+{
+  code: "YOM127",
+  category: "men",
+  image: "images/yom127.jpg"
+},
+
+{
+  code: "YOM128",
+  category: "men",
+  image: "images/yom128.jpg"
+},
+
+{
+  code: "YOM129",
+  category: "men",
+  image: "images/yom129.jpg"
+},
+
+{
+  code: "YOM130",
+  category: "men",
+  image: "images/yom130.jpg"
+},
+{
+  code: "YOM131",
+  category: "men",
+  image: "images/yom131.jpg"
+},
+
+{
+  code: "YOM132",
+  category: "men",
+  image: "images/yom132.jpg"
+},
+
+{
+  code: "YOM133",
+  category: "men",
+  image: "images/yom133.jpg"
+},
+
+{
+  code: "YOM134",
+  category: "men",
+  image: "images/yom134.jpg"
+},
+
+{
+  code: "YOM135",
+  category: "men",
+  image: "images/yom135.jpg"
+},
+
+{
+  code: "YOM136",
+  category: "men",
+  image: "images/yom136.jpg"
+},
+
+{
+  code: "YOM137",
+  category: "men",
+  image: "images/yom137.jpg"
+},
+
+{
+  code: "YOM138",
+  category: "men",
+  image: "images/yom138.jpg"
+},
+
+{
+  code: "YOM139",
+  category: "men",
+  image: "images/yom139.jpg"
+},
+
+{
+  code: "YOM140",
+  category: "men",
+  image: "images/yom140.jpg"
+},
+{
+  code: "YOM141",
+  category: "men",
+  image: "images/yom141.jpg"
+},
+
+{
+  code: "YOM142",
+  category: "men",
+  image: "images/yom142.jpg"
+},
+
+{
+  code: "YOM143",
+  category: "men",
+  image: "images/yom143.jpg"
+},
+
+{
+  code: "YOM144",
+  category: "men",
+  image: "images/yom144.jpg"
+},
+
+{
+  code: "YOM145",
+  category: "men",
+  image: "images/yom145.jpg"
+},
+
+{
+  code: "YOM146",
+  category: "men",
+  image: "images/yom146.jpg"
+},
+
+{
+  code: "YOM147",
+  category: "men",
+  image: "images/yom147.jpg"
+},
+
+{
+  code: "YOM148",
+  category: "men",
+  image: "images/yom148.jpg"
+},
+
+{
+  code: "YOM149",
+  category: "men",
+  image: "images/yom149.jpg"
+},
+
+{
+  code: "YOM150",
+  category: "men",
+  image: "images/yom150.jpg"
+},
+{
+  code: "YOM151",
+  category: "men",
+  image: "images/yom151.jpg"
+},
+
+{
+  code: "YOM152",
+  category: "men",
+  image: "images/yom152.jpg"
+},
+
+{
+  code: "YOM153",
+  category: "men",
+  image: "images/yom153.jpg"
+},
+
+{
+  code: "YOM154",
+  category: "men",
+  image: "images/yom154.jpg"
+},
+
+{
+  code: "YOM155",
+  category: "men",
+  image: "images/yom155.jpg"
+},
+
+{
+  code: "YOM156",
+  category: "men",
+  image: "images/yom156.jpg"
+},
+
+{
+  code: "YOM157",
+  category: "men",
+  image: "images/yom157.jpg"
+},
+
+{
+  code: "YOM158",
+  category: "men",
+  image: "images/yom158.jpg"
+},
+
+{
+  code: "YOM159",
+  category: "men",
+  image: "images/yom159.jpg"
+},
+
+{
+  code: "YOM160",
+  category: "men",
+  image: "images/yom160.jpg"
+},
+{
+  code: "YOM161",
+  category: "men",
+  image: "images/yom161.jpg"
+},
+
+{
+  code: "YOM162",
+  category: "men",
+  image: "images/yom162.jpg"
+},
+
+{
+  code: "YOM163",
+  category: "men",
+  image: "images/yom163.jpg"
+},
+
+{
+  code: "YOM164",
+  category: "men",
+  image: "images/yom164.jpg"
+},
+
+{
+  code: "YOM165",
+  category: "men",
+  image: "images/yom165.jpg"
+},
+
+{
+  code: "YOM166",
+  category: "men",
+  image: "images/yom166.jpg"
+},
+
+{
+  code: "YOM167",
+  category: "men",
+  image: "images/yom167.jpg"
+},
+
+{
+  code: "YOM168",
+  category: "men",
+  image: "images/yom168.jpg"
+},
+
+{
+  code: "YOM169",
+  category: "men",
+  image: "images/yom169.jpg"
+},
+
+{
+  code: "YOM170",
+  category: "men",
+  image: "images/yom170.jpg"
+},
+{
+  code: "YOM171",
+  category: "men",
+  image: "images/yom171.jpg"
+},
+
+{
+  code: "YOM172",
+  category: "men",
+  image: "images/yom172.jpg"
+},
+
+{
+  code: "YOM173",
+  category: "men",
+  image: "images/yom173.jpg"
+},
+
+{
+  code: "YOM174",
+  category: "men",
+  image: "images/yom174.jpg"
+},
+
+{
+  code: "YOM175",
+  category: "men",
+  image: "images/yom175.jpg"
+},
+
+{
+  code: "YOM176",
+  category: "men",
+  image: "images/yom176.jpg"
+},
+
+{
+  code: "YOM177",
+  category: "men",
+  image: "images/yom177.jpg"
+},
+
+{
+  code: "YOM178",
+  category: "men",
+  image: "images/yom178.jpg"
+},
+
+{
+  code: "YOM179",
+  category: "men",
+  image: "images/yom179.jpg"
+},
+
+{
+  code: "YOM180",
+  category: "men",
+  image: "images/yom180.jpg"
+},
+{
+  code: "YOM181",
+  category: "men",
+  image: "images/yom181.jpg"
+},
+
+{
+  code: "YOM182",
+  category: "men",
+  image: "images/yom182.jpg"
+},
+
+{
+  code: "YOM183",
+  category: "men",
+  image: "images/yom183.jpg"
+},
+
+{
+  code: "YOM184",
+  category: "men",
+  image: "images/yom184.jpg"
+},
+
+{
+  code: "YOM185",
+  category: "men",
+  image: "images/yom185.jpg"
+},
+
+{
+  code: "YOM186",
+  category: "men",
+  image: "images/yom186.jpg"
+},
+
+{
+  code: "YOM187",
+  category: "men",
+  image: "images/yom187.jpg"
+},
+
+{
+  code: "YOM188",
+  category: "men",
+  image: "images/yom188.jpg"
+},
+
+{
+  code: "YOM189",
+  category: "men",
+  image: "images/yom189.jpg"
+},
+
+{
+  code: "YOM190",
+  category: "men",
+  image: "images/yom190.jpg"
+},
+{
+  code: "YOM191",
+  category: "men",
+  image: "images/yom191.jpg"
+},
+
+{
+  code: "YOM192",
+  category: "men",
+  image: "images/yom192.jpg"
+},
+
+{
+  code: "YOM193",
+  category: "men",
+  image: "images/yom193.jpg"
+},
+
+{
+  code: "YOM194",
+  category: "men",
+  image: "images/yom194.jpg"
+},
+
+{
+  code: "YOM195",
+  category: "men",
+  image: "images/yom195.jpg"
+},
+
+{
+  code: "YOM196",
+  category: "men",
+  image: "images/yom196.jpg"
+},
+
+{
+  code: "YOM197",
+  category: "men",
+  image: "images/yom197.jpg"
+},
+
+{
+  code: "YOM198",
+  category: "men",
+  image: "images/yom198.jpg"
+},
+
+{
+  code: "YOM199",
+  category: "men",
+  image: "images/yom199.jpg"
+},
+
+{
+  code: "YOM200",
+  category: "men",
+  image: "images/yom200.jpg"
+},
+ {
+  code: "YOM201",
+  category: "perfume",
+  image: "images/yom201.jpg"
+},
+
+{
+  code: "YOM202",
+  category: "perfume",
+  image: "images/yom202.jpg"
+},
+
+{
+  code: "YOM203",
+  category: "perfume",
+  image: "images/yom203.jpg"
+},
+
+{
+  code: "YOM204",
+  category: "perfume",
+  image: "images/yom204.jpg"
+},
+
+{
+  code: "YOM205",
+  category: "perfume",
+  image: "images/yom205.jpg"
+},
+
+{
+  code: "YOM206",
+  category: "perfume",
+  image: "images/yom206.jpg"
+},
+
+{
+  code: "YOM207",
+  category: "perfume",
+  image: "images/yom207.jpg"
+},
+
+{
+  code: "YOM208",
+  category: "perfume",
+  image: "images/yom208.jpg"
+},
+
+{
+  code: "YOM209",
+  category: "perfume",
+  image: "images/yom209.jpg"
+},
+
+{
+  code: "YOM210",
+  category: "perfume",
+  image: "images/yom210.jpg"
+},
+{
+  code: "YOM211",
+  category: "perfume",
+  image: "images/yom211.jpg"
+},
+
+{
+  code: "YOM212",
+  category: "perfume",
+  image: "images/yom212.jpg"
+},
+
+{
+  code: "YOM213",
+  category: "perfume",
+  image: "images/yom213.jpg"
+},
+
+{
+  code: "YOM214",
+  category: "perfume",
+  image: "images/yom214.jpg"
+},
+
+{
+  code: "YOM215",
+  category: "perfume",
+  image: "images/yom215.jpg"
+},
+
+{
+  code: "YOM216",
+  category: "perfume",
+  image: "images/yom216.jpg"
+},
+
+{
+  code: "YOM217",
+  category: "perfume",
+  image: "images/yom217.jpg"
+},
+
+{
+  code: "YOM218",
+  category: "perfume",
+  image: "images/yom218.jpg"
+},
+
+{
+  code: "YOM219",
+  category: "perfume",
+  image: "images/yom219.jpg"
+},
+
+{
+  code: "YOM220",
+  category: "perfume",
+  image: "images/yom220.jpg"
+},
+{
+  code: "YOM221",
+  category: "perfume",
+  image: "images/yom221.jpg"
+},
+
+{
+  code: "YOM222",
+  category: "perfume",
+  image: "images/yom222.jpg"
+},
+
+{
+  code: "YOM223",
+  category: "perfume",
+  image: "images/yom223.jpg"
+},
+
+{
+  code: "YOM224",
+  category: "perfume",
+  image: "images/yom224.jpg"
+},
+
+{
+  code: "YOM225",
+  category: "perfume",
+  image: "images/yom225.jpg"
+},
+
+{
+  code: "YOM226",
+  category: "perfume",
+  image: "images/yom226.jpg"
+},
+
+{
+  code: "YOM227",
+  category: "perfume",
+  image: "images/yom227.jpg"
+},
+
+{
+  code: "YOM228",
+  category: "perfume",
+  image: "images/yom228.jpg"
+},
+
+{
+  code: "YOM229",
+  category: "perfume",
+  image: "images/yom229.jpg"
+},
+
+{
+  code: "YOM230",
+  category: "perfume",
+  image: "images/yom230.jpg"
+},
+{
+  code: "YOM231",
+  category: "perfume",
+  image: "images/yom231.jpg"
+},
+
+{
+  code: "YOM232",
+  category: "perfume",
+  image: "images/yom232.jpg"
+},
+
+{
+  code: "YOM233",
+  category: "perfume",
+  image: "images/yom233.jpg"
+},
+
+{
+  code: "YOM234",
+  category: "perfume",
+  image: "images/yom234.jpg"
+},
+
+{
+  code: "YOM235",
+  category: "perfume",
+  image: "images/yom235.jpg"
+},
+
+{
+  code: "YOM236",
+  category: "perfume",
+  image: "images/yom236.jpg"
+},
+
+{
+  code: "YOM237",
+  category: "perfume",
+  image: "images/yom237.jpg"
+},
+
+{
+  code: "YOM238",
+  category: "perfume",
+  image: "images/yom238.jpg"
+},
+
+{
+  code: "YOM239",
+  category: "perfume",
+  image: "images/yom239.jpg"
+},
+
+{
+  code: "YOM240",
+  category: "perfume",
+  image: "images/yom240.jpg"
+},
+{
+  code: "YOM241",
+  category: "perfume",
+  image: "images/yom241.jpg"
+},
+
+{
+  code: "YOM242",
+  category: "perfume",
+  image: "images/yom242.jpg"
+},
+
+{
+  code: "YOM243",
+  category: "perfume",
+  image: "images/yom243.jpg"
+},
+
+{
+  code: "YOM244",
+  category: "perfume",
+  image: "images/yom244.jpg"
+},
+
+{
+  code: "YOM245",
+  category: "perfume",
+  image: "images/yom245.jpg"
+},
+
+{
+  code: "YOM246",
+  category: "perfume",
+  image: "images/yom246.jpg"
+},
+
+{
+  code: "YOM247",
+  category: "perfume",
+  image: "images/yom247.jpg"
+},
+
+{
+  code: "YOM248",
+  category: "perfume",
+  image: "images/yom248.jpg"
+},
+
+{
+  code: "YOM249",
+  category: "perfume",
+  image: "images/yom249.jpg"
+},
+
+{
+  code: "YOM250",
+  category: "perfume",
+  image: "images/yom250.jpg"
+},
